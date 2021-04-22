b86dce0e7b45768278ef1895a1a159b1205653ff
tayarthouail <tayarthouail@gmail.com>
2021-04-23 00:58:15 +0200

    Fetching the data done & the UI done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+
+const [jokes, setJokes] = useState();
+
+//function to display what jokes on the UI
+useEffect(() => {
+
+  const getFetchJokes = async () => {
+      const res = await fetchJokes(); 
+      setJokes(res);
+      console.log(res);
+  }
+  getFetchJokes();
+ 
+}, [])
+
+
+// Function to fetch jokes and use it on Click event handler
+const fetchJokes = async () => {
+    const getJokes = await axios.get('https://api.chucknorris.io/jokes/random');
+    const {value} = getJokes.data;
+    setJokes(value);
+    return value;
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <div className="card-center">
+         <h4>Chuck Norris Jokes</h4>
+        <h1 className="title">{jokes}</h1>
+        <button className="btn" onClick={fetchJokes}>New Joke</button>
+        </div>
+      </div>
   );
 }
 
